d05dd3f57c8612f79cb33a0ad0f567edf9fd1bcb
Phat Tran <PHAT@Phat.local>
2017-07-11 13:19:40 +0700

    manage state with setState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super(); // 'this' keyword gets reference to this component
+    this.state = {
+      text: 'this is the state text',
+      cat: 0
+    }
+  }
+
+  update(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
-    let text = this.props.text;
     return (
       <div>
-        <h1>{text}</h1>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.text} - {this.state.cat}</h1>
       </div>
     )
   }
 }
-
-App.propTypes = {
-  text: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  text: 'default text'
-};
 
 export default App;
